ccaeb0cc41eb780b82925ea9ac5203a413e40aa6
lilfool1994 <99862002+lilfool1994@users.noreply.github.com>
2022-03-25 12:11:54 -0700

    Update TemplateMyBag.h

--- TemplateMyBag.h
+++ TemplateMyBag.h
@@ -1,1 +1,47 @@
+int TemplateClassMenuOption()
 
+{
+    cout << endl << "1> Template MyBag of double";
+    cout << endl << "==========================================";
+    cout << endl << "1 > clear";
+    cout << endl << "2 > insert";
+    cout << endl << "3 > search";
+    cout << endl << "4 > remove";
+    cout << endl << "5 > sort";
+    cout << endl << "6 > display";
+
+    cout << endl << "==========================================";
+    cout << endl << "0. Exit";
+    cout << endl << "==========================================" << endl;
+    int options = inputInteger("Options: ", 0, 6);
+    cout << endl << endl << endl;
+    return options;
+}
+
+
+
+
+void main()
+{
+    
+   
+ 
+    do
+    {
+        switch (TemplateClassMenuOption())
+        {
+        case 0: return; break;
+             //  case 1: clear(); break;
+             //  case 2:  insert(); break;
+             //  case 3: search(); break;
+             //  case 4: remove(); break;
+             //  case 5: sortArray(); break;
+              // case 6: display(); break;
+        default: cout << "\t\tERROR - Invalid option. Please re-enter."; break;
+        }
+        cout << "\n";
+        system("pause");
+
+    } while (true);
+
+}
