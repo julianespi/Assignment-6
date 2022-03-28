f0515a3f316beda564bda06326ced61f015f29e7
WesleyGoo <99233770+WesleyGoo@users.noreply.github.com>
2022-03-28 09:18:52 -0700

    Add files via upload

--- TemplateMyBag.h
+++ TemplateMyBag.h
@@ -1,3 +1,120 @@
+#include <iostream>
+#include <stack>
+#include <iomanip>
+#include <iterator>
+#include <cmath>
+#include <string>
+#include <list>
+#include <cstring>
+#include <sstream>
+#include <chrono>
+#include <vector>
+#include <fstream>
+#include "input.h"
+
+using namespace std;
+
+
+template <class T>
+class MyBag {
+private:
+    T* array;
+    int size;
+    int index;
+public:
+    MyBag()
+    {
+        array = new T[10];
+        size = 0;
+        index = 0;
+    }
+    MyBag(T* a, int newSize)
+    {
+        size = newSize;
+        array = new T[10];
+
+        for (int i = 0; i < size; i++)
+            array[i] = a[i];
+    }
+    ~MyBag()
+    {
+        delete[] array;
+    }
+
+    //Precondition: user input to display array
+    //Postcondition: displays current array setup
+
+    void display()
+    {
+        cout << endl << "Displaying array: \n";
+
+        for (int i = 0; i < size; i++)
+            cout << array[i] << '\n';
+
+
+    }
+
+    //Precondition: user input to enter new integer into array
+    //Postcondition: places an integer, then adds to the dynamic array size
+    void insert()
+    {
+        T num = { inputDouble("Enter new integer into array: ") };
+        array[index] = num;
+        index++;
+        size++;
+    }
+
+    //Precondition: user input to remove a certain array
+    //Postcondition: removes the array, and subtracts to the dynamic array
+    void remove()
+    {
+
+        int remove = { inputInteger("Enter position to remove: ") };
+
+        for (int i = remove; i < size - 1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+        array[index - 1] = {};
+        size--;
+    }
+    //Precondition: user input to search an integer 
+    //Postcondition: displays the integer found at specific index
+    void search()
+    {
+        int SearchArray = inputInteger("Search: ");
+        for (int i = 0; i < size; i++) {
+            if (array[i] == SearchArray) {
+                cout << "Element found at index " << i;
+            }
+        }
+
+    }
+
+    //Precondition: user input to sort current array
+    //Postcondition: sorts array then displays sorted array
+    void sortArray()
+    {
+        cout << endl << "The sorted array is: \n";
+
+        sort(array, array + size);
+        for (int i = 0; i < size; i++)
+            cout << array[i] << '\n';
+    }
+
+
+    //Precondition: user input to clear the array
+    //Postcondition: clears the array
+    void clear()
+    {
+        size = 0;
+        cout << endl << "The array has been cleared. \n";
+    }
+
+
+
+};
+
 int TemplateClassMenuOption()
 
 {
@@ -23,20 +140,18 @@ int TemplateClassMenuOption()
 
 void TemplateMyBag()
 {
-    
-   
- 
+    MyBag<double> h;
     do
     {
         switch (TemplateClassMenuOption())
         {
         case 0: return; break;
-             //  case 1: clear(); break;
-             //  case 2:  insert(); break;
-             //  case 3: search(); break;
-             //  case 4: remove(); break;
-             //  case 5: sortArray(); break;
-              // case 6: display(); break;
+        case 1: h.clear(); break;
+        case 2: h.insert(); break;
+        case 3: h.search(); break;
+        case 4: h.remove(); break;
+        case 5: h.sortArray(); break;
+        case 6: h.display(); break;
         default: cout << "\t\tERROR - Invalid option. Please re-enter."; break;
         }
         cout << "\n";
